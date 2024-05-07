a5aa2b8bdcf8415a6aa0dd270de37d913e780edd
shahzad147 <160399281+shahzad147@users.noreply.github.com>
2024-05-07 09:51:46 +0500

    created simple calculator

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const ans = await inquirer.prompt([{
+    type: "number",
+    name: "first_number",
+    message: "enter your first number: "
+},
+{
+    type: "number",
+    name: "second_number",
+    message: "enter your second number: "
+},
+{
+    type: "list",
+    name: "operator",
+    message: "please select operation type: ",
+    choices: ["Addition","Subtraction","Multiplication","Division"]
+}
+])
+
+if(ans.operator === "Addition"){
+    console.log(ans.first_number + ans.second_number);    
+}
+else if(ans.operator === "Subtraction"){
+    console.log(ans.first_number - ans.second_number);    
+}
+else if(ans.operator === "Multiplication"){
+    console.log(ans.first_number * ans.second_number);    
+}
+else{
+    console.log(ans.first_number / ans.second_number);    
+}
